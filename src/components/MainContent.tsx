38aba64db1322afb7ac6f953acd31d214c41c659
zieeco <zieecosam@gmail.com>
2024-01-09 17:35:45 +0100

    feat(maincontent): enhance functionality in MainContent.tsx

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,6 +1,6 @@
+// MainContent.tsx
 'use client';
 
-// MainContent.tsx
 import { useState, useEffect } from 'react';
 import CountdownTimer from './CountdownTimer';
 import axios from 'axios';
@@ -8,13 +8,14 @@ import axios from 'axios';
 const MainContent: React.FC = () => {
   const [email, setEmail] = useState('');
   const [isSubscribed, setIsSubscribed] = useState(false);
-  const [targetDate, setTargetDate] = useState<Date | null>(null);
+  const [initialTargetDate, setInitialTargetDate] = useState<Date | null>(null);
 
   useEffect(() => {
     const fetchTargetDate = async () => {
       try {
         const response = await axios.get('/api/countdownEndTime');
-        setTargetDate(new Date(response.data.targetDate));
+        const newTargetDate = new Date(response.data.targetDate);
+        setInitialTargetDate(newTargetDate);
       } catch (error: any) {
         console.error('Error fetching target date:', error.message);
       }
@@ -41,38 +42,45 @@ const MainContent: React.FC = () => {
   };
 
   return (
-    <div className="container">
-      <div className="countdown-container">
-        <h1 className="text-3xl font-bold mb-4">Coming Soon!</h1>
-        {targetDate ? (
-          <CountdownTimer targetDate={targetDate} />
+    <div className="w-full flex flex-col h-screen text-center p-8 bbb justify-center items-center">
+      <div className="one w-full flex-col mborder items-center mx-auto  md:max-w-2xl">
+        <div className="countdown-container mb-12">
+          <h1 className="all-font font-bold uppercase md:text-3xl">This website is under construction!</h1>
+          {/* <h4 className="all-font uppercase">We will be live in:</h4> */}
+        </div>
+
+        {isSubscribed ? (
+          <p className="text-green-500">You are subscribed! Thank you.</p>
         ) : (
-          <p>Loading...</p>
+          <form className="form-container" onSubmit={handleSubscribe}>
+            {/* <p className="all-font uppercase">Enter your email address to receive updates</p> */}
+            <p className="flex flex-col justify-center items-center sm:flex-row gap-4">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+                className="border br border-gray-300 p-2 w-full"
+                placeholder="EMAIL ADDRESS"
+              />
+              <button type="submit"
+                className="bg-blue-500 text-white px-4 py-2 hover:bg-blue-600 br
+                mx-auto sm:mx-0"
+                disabled={isSubscribed}>
+                Subscribe
+              </button>
+            </p>
+          </form>
+        )}
+
+        {initialTargetDate ? (
+          <CountdownTimer initialTargetDate={initialTargetDate} />
+        ) : (
+        <p className="w-12 h-12 my-5 rounded-full animate-spin mx-auto border-4 border-solid border-gray-500 border-t-transparent"></p>
         )}
       </div>
-
-      {isSubscribed ? (
-        <p className="text-green-500">You are subscribed! Thank you.</p>
-      ) : (
-        <form className="" onSubmit={handleSubscribe}>
-          <p className="">Enter your email address to receive updates</p>
-          <p>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              required
-              onChange={(e) => setEmail(e.target.value)}
-              className="border border-gray-300 p-2 rounded-md mb-4"
-              placeholder="EMAIL ADDRESS"
-            />
-            <button type="submit" className="subscribe-btn" disabled={isSubscribed}>
-              Subscribe
-            </button>
-          </p>
-        </form>
-      )}
     </div>
   );
 };
