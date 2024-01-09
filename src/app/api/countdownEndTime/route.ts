ba5bd33e4f410ba5ca79539f849e3399cd807537
zieeco <zieecosam@gmail.com>
2024-01-09 17:43:23 +0100

    chore(api): enhance countdown timer calculation in API

--- src/app/api/countdownEndTime/route.ts
+++ src/app/api/countdownEndTime/route.ts
@@ -1,17 +1,21 @@
 // pages/api/countdownEndTime/route.ts
 import { NextResponse, NextRequest } from 'next/server';
 
+let targetDate: Date | null = null;
+
 export const GET = async (request: NextRequest ) => {
   if (request.method !== 'GET') {
     return NextResponse.json({ error: 'Method not allowed!' }, { status: 405 });
   }
 
   try {
-    const now = new Date();
-    const futureDate = new Date();
-    futureDate.setDate(now.getDate() + 30);
+    if (!targetDate) {
+      const now = new Date();
+      targetDate = new Date();
+      targetDate.setDate(now.getDate() + 30);
+    }
 
-    return NextResponse.json({ targetDate: futureDate.toISOString() }, { status: 200 });
+    return NextResponse.json({ targetDate: targetDate.toISOString() }, { status: 200 });
   } catch (error) {
     console.error('Error in target-date API:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
