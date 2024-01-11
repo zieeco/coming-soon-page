9dbef0246675ebd35bacf7178d1da0b01355e4df
zieeco <zieecosam@gmail.com>
2024-01-11 19:05:51 +0100

    refactor(api): update the subcribe route logic

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,16 +1,36 @@
 // app/api/subscribe/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import fs from 'fs/promises';
-import path from 'path';
+import validator from 'validator';
+import{ MongoClient } from 'mongodb'
+
+const MONGODB_URI = process.env.MONGODB_URI || '';
+const DATABASE_NAME = 'subscription';
 
 export const POST = async (request: NextRequest) => {
   try {
     const reqBody = await request.json();
-    console.log("sever::>>>> ", reqBody)
+    console.log("sever::>>>> The user email submitted:: ", reqBody)
     const { email } = reqBody;
 
-    const filePath = path.join(process.cwd(), 'subscribers.txt');
-    await fs.appendFile(filePath,  `${email}\n`);
+    if (!validator.isEmail(email)) {
+      return NextResponse.json({ error: 'Invalid Email format!'}, { status: 400 });
+    }
+
+    const client = new MongoClient(MONGODB_URI, {});
+    await client.connect();
+
+    const database = client.db(DATABASE_NAME);
+    const collection = database.collection('subscribers');
+
+    const existingSubscriber = await collection.findOne({ email });
+    if (existingSubscriber) {
+      const errorMessage = `${email} is already subscribed!`;
+      console.error("You have already subscribed::::", errorMessage);
+      return NextResponse.json({ error: errorMessage }, { status: 400 });
+    }
+
+    await collection.insertOne({ email });
+    await client.close();
 
     console.log('Server: Subscription successful');
 
@@ -18,4 +38,4 @@ export const POST = async (request: NextRequest) => {
   } catch (error) {
     return NextResponse.json({ error: 'Something went wrong!' }, { status: 500 });
   }
-}
+};
